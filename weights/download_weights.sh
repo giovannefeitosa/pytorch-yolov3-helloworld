63e3db89acc957767bd6a1d505b5fe3f2699f679
Giovanne Afonso <giovanneafonso@gmail.com>
2022-08-03 17:59:38 -0300

    chore: download coco dataset qua 03 ago 2022 17:59:38 -03

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-# Download weights for vanilla YOLOv3
-wget -c "https://pjreddie.com/media/files/yolov3.weights" --header "Referer: pjreddie.com"
-# # Download weights for tiny YOLOv3
-wget -c "https://pjreddie.com/media/files/yolov3-tiny.weights" --header "Referer: pjreddie.com"
-# Download weights for backbone network
-wget -c "https://pjreddie.com/media/files/darknet53.conv.74" --header "Referer: pjreddie.com"
+
+function main() {
+  local HERE=$(cd $(dirname $0) && pwd -P)
+  local PROJECT_ROOT=$(cd $HERE/.. && pwd -P)
+  cd "$HERE"
+
+  # stop on error
+  set -e
+
+  if [ ! -f "$HERE/yolov3.weights" ]; then
+    echo 'Download weights for vanilla YOLOv3'
+    wget -q -c "https://pjreddie.com/media/files/yolov3.weights" --header "Referer: pjreddie.com"
+  fi
+
+  if [ ! -f "$HERE/yolov3-tiny.weights" ]; then
+    echo 'Download weights for tiny YOLOv3'
+    wget -q -c "https://pjreddie.com/media/files/yolov3-tiny.weights" --header "Referer: pjreddie.com"
+  fi
+
+  if [ ! -f "$HERE/darknet53.conv.74" ]; then
+    echo 'Download weights for backbone network'
+    wget -q -c "https://pjreddie.com/media/files/darknet53.conv.74" --header "Referer: pjreddie.com"
+  fi
+}
+
+main
