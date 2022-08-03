a9f6a97a9744ad45148b890136c32caea8877fdb
Giovanne Afonso <giovanneafonso@gmail.com>
2022-08-03 18:56:41 -0300

    chore: download coco dataset qua 03 ago 2022 18:56:41 -03

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -5,22 +5,45 @@ function main() {
   local PROJECT_ROOT=$(cd $HERE/.. && pwd -P)
   cd "$HERE"
 
+  local CACHE_WEIGHTS_DIR='/tmp/pytorch-yolov3-helloworld/weights'
+  mkdir -p "$CACHE_WEIGHTS_DIR"
+
+  echo "CACHE_WEIGHTS_DIR=$CACHE_WEIGHTS_DIR"
+
   # stop on error
   set -e
 
   if [ ! -f "$HERE/yolov3.weights" ]; then
-    echo 'Download weights for vanilla YOLOv3'
-    wget -q -c "https://pjreddie.com/media/files/yolov3.weights" --header "Referer: pjreddie.com"
+    if [ -f "$CACHE_WEIGHTS_DIR/yolov3.weights" ]; then
+      echo 'Copy weights from cache: yolov3.weights'
+      cp "$CACHE_WEIGHTS_DIR/yolov3.weights" "$HERE/yolov3.weights"
+    else
+      echo 'Download weights for vanilla YOLOv3'
+      wget -q -c "https://pjreddie.com/media/files/yolov3.weights" --header "Referer: pjreddie.com"
+      cp "$HERE/yolov3.weights" "$CACHE_WEIGHTS_DIR/yolov3.weights"
+    fi
   fi
 
   if [ ! -f "$HERE/yolov3-tiny.weights" ]; then
-    echo 'Download weights for tiny YOLOv3'
-    wget -q -c "https://pjreddie.com/media/files/yolov3-tiny.weights" --header "Referer: pjreddie.com"
+    if [ -f "$CACHE_WEIGHTS_DIR/yolov3-tiny.weights" ]; then
+      echo 'Copy weights from cache: yolov3-tiny.weights'
+      cp "$CACHE_WEIGHTS_DIR/yolov3-tiny.weights" "$HERE/yolov3-tiny.weights"
+    else
+      echo 'Download weights for tiny YOLOv3'
+      wget -q -c "https://pjreddie.com/media/files/yolov3-tiny.weights" --header "Referer: pjreddie.com"
+      cp "$HERE/yolov3-tiny.weights" "$CACHE_WEIGHTS_DIR/yolov3-tiny.weights"
+    fi
   fi
 
   if [ ! -f "$HERE/darknet53.conv.74" ]; then
-    echo 'Download weights for backbone network'
-    wget -q -c "https://pjreddie.com/media/files/darknet53.conv.74" --header "Referer: pjreddie.com"
+    if [ -f "$CACHE_WEIGHTS_DIR/darknet53.conv.74" ]; then
+      echo 'Copy weights from cache: darknet53.conv.74'
+      cp "$CACHE_WEIGHTS_DIR/darknet53.conv.74" "$HERE/darknet53.conv.74"
+    else
+      echo 'Download weights for backbone network'
+      wget -q -c "https://pjreddie.com/media/files/darknet53.conv.74" --header "Referer: pjreddie.com"
+      cp "$HERE/darknet53.conv.74" "$CACHE_WEIGHTS_DIR/darknet53.conv.74"
+    fi
   fi
 }
 
